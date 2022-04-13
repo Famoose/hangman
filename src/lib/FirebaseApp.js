0303f8e360cc00420cfa4adfc0e4bbfb9c77112c
lukasnydegger <nydegger.lukas@gmail.com>
2022-04-13 13:02:44 +0200

    add offline support and fix background color

--- src/lib/FirebaseApp.js
+++ src/lib/FirebaseApp.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {initializeApp} from 'firebase/app'
+import {getAuth} from 'firebase/auth'
+import {
+    getFirestore, enableIndexedDbPersistence,
+} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,6 +19,20 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
+
+enableIndexedDbPersistence(db)
+    .catch((err) => {
+        if (err.code === 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled
+            // in one tab at a a time.
+            // ...
+        } else if (err.code === 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+        }
+    })
+// Subsequent queries will use persistence, if it was enabled successfully
 
 export const getUid = () => {
     if (auth.currentUser) {
