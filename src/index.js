311eaa7e8819a24cf282ae0060d2731c9d319b93
lukasnydegger <nydegger.lukas@gmail.com>
2022-04-13 19:07:14 +0200

    fix icon on ios and caching issues

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './i18n';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {enableOfflinePersistence} from "./lib/FirebaseApp";
+enableOfflinePersistence();
 
 ReactDOM.render(
     <BrowserRouter>
@@ -16,7 +17,6 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-enableOfflinePersistence();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
