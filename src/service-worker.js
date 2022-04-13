d0ee5741a9c47c1a065ec530460c91d286bbf41c
lukasnydegger <nydegger.lukas@gmail.com>
2022-04-13 23:03:41 +0200

    cache rive wasm

--- src/service-worker.js
+++ src/service-worker.js
@@ -76,6 +76,19 @@ registerRoute(
 
 registerRoute(
     // Add in any other file extensions or routing criteria as needed.
+    ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.wasm'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    new StaleWhileRevalidate({
+        cacheName: 'wasms',
+        plugins: [
+            // Ensure that once this runtime cache reaches a maximum size the
+            // least-recently used images are removed.
+            new ExpirationPlugin({ maxEntries: 50 }),
+        ],
+    })
+);
+
+registerRoute(
+    // Add in any other file extensions or routing criteria as needed.
     ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.json'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
     new StaleWhileRevalidate({
         cacheName: 'jsons',
